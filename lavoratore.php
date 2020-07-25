9f4737c1af96558e3836fe1c7846dde81c42143c
ciob <vasile.c99@gmail.com>
2020-07-25 11:57:42 +0200

    refresh lavoratore

--- lavoratore.php
+++ lavoratore.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <h1>Lavoratore</h1>
-    <?php
+<?php
     	$conn = pg_connect("host=localhost port=5432 dbname=essecorta user=postgres password=postgres");
 		if (!$conn){
 			die('Connessione al database fallita.');
@@ -24,16 +24,15 @@
 				echo '<br><table class="form">
 				<tr>
 					<th>Cod Fiscale</th>
-					<th>Nome</td>
-					<th>Cognome</th>
+					<th>Nome</th>
+                    <th>Cognome</th>
                     <th>Telefono</th>
-                    <th>Data Nascita</th>
+					<th>Data Nascita</th>
                     <th>E-mail</th>
                     <th>Mansione</th>
-                    <th>Livello</th>
+					<th>Livello</th>
                     <th>Città</th>
                     <th>Via</th>
-                    <th>CAP</th>
                     <th>Data Assunzione</th>
                     <th>Reparto</th>
 				</tr>';
@@ -41,19 +40,18 @@
 					echo '<tr>
 						<td>'. $row['codfiscale'].'</td>
 						<td>'. $row['nome'].'</td>
-						<td>'. $row['cognome'].'</td>
+                        <td>'. $row['cognome'].'</td>
                         <td>'. $row['telefono'].'</td>
-                        <td>'. $row['datanascita'].'</td>
+						<td>'. $row['datanascita'].'</td>
                         <td>'. $row['email'].'</td>
                         <td>'. $row['mansione'].'</td>
-                        <td>'. $row['livello'].'</td>
+						<td>'. $row['livello'].'</td>
                         <td>'. $row['citta'].'</td>
-                        <td>'. $row['via'].', '.$row['nciv'].'</td>
-                        <td>'. $row['cap'].'</td>
+                        <td>'. $row['via'].', '. $row['nciv'].', ('. $row['cap'].')'.'</td>
                         <td>'. $row['dataassunzione'].'</td>
                         <td>'. $row['reparto'].'</td>
 					</tr>';
-				};
+				}
 				echo '</table>';
 			}
         }
@@ -63,20 +61,19 @@
         print("<table class=\"form\">");
         print("<form action=\"".htmlspecialchars($_SERVER['PHP_SELF'])."\" method=\"POST\">");
         print("<tr><th>Cod Fiscale</th><td><input type=\"text\" name=\"codfiscale\" pattern=\".{13,}\" title=\"CF non idoneo\"></td>");
-        print("    <th>Nome</th><td><input type=\"text\" name=\"nome\"</td>");
-        print("    <th>Cognome</th><td><input type=\"text\" name=\"cognome\" </td>");
-        print("    <th>Telefono</th><td><input type=\"text\" name=\"telefono\" pattern=\"[0-9].{6,10}\" title=\"only digits\"></td></tr>");
+        print("<th>Nome</th><td><input type=\"text\" name=\"nome\"</td>");
+        print("<th>Cognome</th><td><input type=\"text\" name=\"cognome\" </td>");
+        print("<th>Telefono</th><td><input type=\"text\" name=\"telefono\" pattern=\"[0-9].{6,10}\" title=\"only digits\"></td></tr>");
         print("<tr><th>Data Nascita</th><td><input type=\"date\" name=\"datanascita\" ></td>");
-        print("    <th>E-mail</th><td><input type=\"text\" name=\"email\" pattern=\"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$\" title=\"no valid value\"></td>");
-        print("    <th>Mansione</th><td><input type=\"text\" name=\"mansione\" ></td>");
-        print("    <th>Livello</th><td><input type=\"text\" name=\"livello\" pattern=\"[1-5]\" title=\"no valid value\"></td></tr>");
+        print("<th>E-mail</th><td><input type=\"text\" name=\"email\" pattern=\"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$\" title=\"no valid value\"></td>");
+        print("<th>Mansione</th><td><input type=\"text\" name=\"mansione\" ></td>");
+        print("<th>Livello</th><td><input type=\"text\" name=\"livello\" pattern=\"[1-5]\" title=\"no valid value\"></td></tr>");
         print("<tr><th>Città</th><td><input type=\"text\" name=\"citta\" ></td>");
-        print("    <th>Via</th><td><input type=\"text\" name=\"via\" ></td>");
-        print("    <th>Civico</th><td><input type=\"text\" name=\"nciv\" ></td>");
-        print("    <th>CAP</th><td><input type=\"text\" name=\"cap\" ></td></tr>");
+        print("<th>Via</th><td><input type=\"text\" name=\"via\" ></td>");
+        print("<th>Civico</th><td><input type=\"text\" name=\"nciv\" ></td>");
+        print("<th>CAP</th><td><input type=\"text\" name=\"cap\" ></td></tr>");
         print("<tr><th>Data Assunzione</th><td><input type=\"date\" name=\"dataassunzione\" ></td>");
-
-        print("    <th>Reparto</th><td colspan=\"2\"> <select name=\"reparto\" id=\"reparto\"><option value=\"\"></option>");
+        print("<th>Reparto</th><td colspan=\"2\"> <select name=\"reparto\" id=\"reparto\"><option value=\"\"></option>");
         $query="SELECT id,nome,supermercato FROM Reparto";
 		$result =  pg_query($conn, $query);
         while ($row = pg_fetch_array($result)) {
@@ -108,8 +105,11 @@
                 VALUES ('$codfiscale','$nome','$cognome','$telefono','$datanascita','$email','$mansione','$livello','$citta','$via','$nciv','$cap','$dataassunzione','$reparto') ";
 			$result = pg_query($conn,$query);
 			if ($result){
-                echo "funziona invio DA SISTEMARE";
-				//header('Location: lavoratore.php');
+                echo "<script>
+                if(window.location.href.substr(-2) !== \"?r\") {
+                     window.location = window.location.href + \"?r\";
+                }
+                </script>";
 			}else{
 					echo "Si è verificato un errore.<br/>";
 					echo pg_last_error($conn);
@@ -132,13 +132,16 @@
 			$query="DELETE FROM Lavoratore WHERE codfiscale='$codfiscale'";
 			$result = pg_query($conn,$query);
 			if ($result){
-                echo "funziona ELIMINA DA SISTEMARE";
-				//header('Location: reparto.php');
+				echo "<script>
+                    if(window.location.href.substr(-2) !== \"\") {
+                         window.location = window.location.href + \"\";
+                    }
+                    </script>";
 			}else{
 					echo "Si è verificato un errore.<br/>";
 					echo pg_last_error($conn);
 			}
 		}
-    ?>
+?>
 </body>
 </html>
