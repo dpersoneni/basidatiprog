686c011415a4e35b91a1955d7b4c589ca3d1d534
danilo <personenidanilo@gmail.com>
2020-07-26 10:25:26 +0200

    maybe finished fornitore

--- fornitore.php
+++ fornitore.php
@@ -73,13 +73,13 @@
 			if( isset($_POST['idata']) and $_POST['idata']=='Filter' and isset($_POST['piva']) and $_POST['piva']!='') {
 
 				print("<table class=\"form\">");
-				print("<tr><th>Prodotto</th><th>Fornitore</th><th>Giorni di consegna</th><th>Prezzo del fornitore</th><th>Codice prodotto dal fornitore</th></tr>");
-				$querydispo = "SELECT prodotto, fornitore, giorniconsegna, prezzofornitore, codprodfornitore FROM Disponibilita WHERE fornitore='".$_POST['piva']."'";
+				print("<tr><th>Prodotto</th><th>Giorni di consegna</th><th>Prezzo del fornitore</th><th>Codice prodotto dal fornitore</th></tr>");
+				$querydispo = "SELECT prodotto, giorniconsegna, prezzofornitore, codprodfornitore FROM Disponibilita WHERE fornitore='".$_POST['piva']."'";
 				$resultdispo = pg_query($conn, $querydispo);
 				
 				while ($rowdispo = pg_fetch_array($resultdispo)) {
                     
-                    print("<tr><td>$rowdispo[prodotto]</td><td>$rowdispo[fornitore]</td><td>$rowdispo[giorniconsegna]</td><td>$rowdispo[prezzofornitore]</td><td>$rowdispo[codprodfornitore]</td><td>");
+                    print("<tr><td>$rowdispo[prodotto]</td><td>$rowdispo[giorniconsegna]</td><td>$rowdispo[prezzofornitore]</td><td>$rowdispo[codprodfornitore]</td><td>");
                     
                     /*
                     $queryorari = "SELECT orainizio, orafine, giorno FROM Turno  WHERE Lavoratore='".$rowpersonale['codfiscale']."'";
@@ -91,7 +91,53 @@
                     */
 					print("</td>");
 				}
-				print("</tr>");
+                print("</tr>");
+                print("</table>");
+
+			}
+        }
+        
+        //INSERIMENTO disponibilità
+        print("<h2>Inserimento prodotti disponibili del fornitore</h2>");
+        print("<table class=\"form\">");
+		print("<form action=\"".htmlspecialchars($_SERVER['PHP_SELF'])."\" method=\"POST\">");
+        print("<th>Fornitore</th> <td><select name=\"fornitore\" id=\"fornitore\"><option value=\"\"></option>");
+        $query="SELECT piva FROM Fornitore";
+        $result = pg_query($conn, $query);
+        while ($row = pg_fetch_array($result)) {
+            print("<option value=\"$row[piva]\">$row[piva]</option>");
+        }
+        print("</td></tr>");       
+        print("<th>Prodotto</th> <td><select name=\"prodotto\" id=\"prodotto\"><option value=\"\"></option>");
+        $query="SELECT codbarre FROM Prodotto";
+        $result = pg_query($conn, $query);
+        while ($row = pg_fetch_array($result)) {
+            print("<option value=\"$row[codbarre]\">$row[codbarre]</option>");
+        }
+        print("</td></tr>");   
+        print("<tr><th>Tempo di consegna</th><td><input type=\"text\" name=\"giorniconsegna\"></td></tr>");
+        print("<tr><th>Prezzo del fornitore</th><td><input type=\"text\" name=\"prezzofornitore\"></td></tr>");
+        print("<tr><th>Codice prodotto del fornitore</th><td><input type=\"text\" name=\"codprodfornitore\"></td></tr>");
+		//print("<input type=\"hidden\" name=\"citta\" value=\"Milano\">");
+        print("<tr><td><input type=\"submit\" name=\"idata\" value=\"Send\"></td></tr>");
+        print("</form>");
+		print("</table>"); 
+		
+		if( isset($_POST['idata']) and $_POST['idata']=='Send') {   
+
+			$fornitore=isset($_POST['fornitore'])?$_POST['fornitore']:'';
+			$prodotto=isset($_POST['prodotto'])?$_POST['prodotto']:'';
+			$giorniconsegna=isset($_POST['giorniconsegna'])?$_POST['giorniconsegna']:'';
+            $prezzofornitore=isset($_POST['prezzofornitore'])?$_POST['prezzofornitore']:'';
+            $codprodfornitore=isset($_POST['codprodfornitore'])?$_POST['codprodfornitore']:'';
+
+			$query="INSERT INTO disponibilita (fornitore, prodotto, giorniconsegna, prezzofornitore, codprodfornitore) VALUES ('$fornitore','$prodotto','$giorniconsegna','$prezzofornitore','$codprodfornitore')";
+			$result = pg_query($conn,$query);
+			if ($result){
+				header('Location: fornitore.php');
+			}else{
+				echo "Si è verificato un errore.<br/>";
+				echo pg_last_error($conn);
 			}
 		}
 
